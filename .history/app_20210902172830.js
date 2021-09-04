6ba15ed3742a0b39eb4159593eebfe8496703b75
Devesh Kumar <deveshkumar2107@hotmail.com>
2021-09-04 16:35:09 -0400

    added multer

---
+++ .history/app_20210902172830.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+const placesRoutes = require("./routes/places-routes");
+const usersRoutes = require("./routes/users-routes");
+const HttpError = require("./models/http-error");
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true
+  })
+);
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  next();
+});
+
+app.use("/api/places", placesRoutes);
+app.use("/api/users", usersRoutes);
+
+app.use((req, res, next) => {
+  const error = new HttpError("Could not find this route", 404);
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || "An unknown error occurred!" });
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://deveshk:deveshk@fullstackcluster.mtcih.mongodb.net/mern?retryWrites=true&w=majority"
+  )
+  .then(() => app.listen(5000))
+  .catch((err) => console.log(err));
---
+++ .history/routes/places-routes_20210903131853.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const router = express.Router();
+const { check } = require("express-validator");
+
+const HttpError = require("../models/http-error");
+const {
+  getPlaceByPlaceId,
+  getPlaceByUserId: getPlacesByUserId,
+  createPlace,
+  updatePlace,
+  deletePlace
+} = require("../controller/places-controller");
+
+router.get("/:pid", getPlaceByPlaceId);
+
+router.get("/user/:uid", getPlacesByUserId);
+
+router.post(
+  "/",
+  // [
+  //   check("title").isLength({ min: 3 }),
+  //   check("description").isLength({ min: 5 }),
+  //   check("address").not().isEmpty()
+  // ],
+  createPlace
+);
+
+router.patch(
+  "/:pid",
+  [
+    check("title").isLength({ min: 3 }),
+    check("description").isLength({ min: 5 })
+  ],
+  updatePlace
+);
+
+router.delete("/:pid", deletePlace);
+
+module.exports = router;
